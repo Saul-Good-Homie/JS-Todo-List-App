2826e19cd99c0b0f24db7f8aaa0e6d8bebd52ea7
Saul Belisle <rbelisle1270@gmail.com>
2021-02-27 13:42:48 -0800

    created basic todo Class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 console.log("Hi, this is a npx webpack test message");
+
+import ToDo from "./todo";
---
+++ src/todo.js
@@ -0,0 +1,48 @@
+export default class ToDo {
+	constructor(name, description, dueDate, priority) {
+		this.name = name;
+		this.description = description;
+		this.dueDate = dueDate;
+		this.priority = priority;
+	}
+
+	get name() {
+		return this._name;
+	}
+
+	set name(newName) {
+		if (newName.length < 1) {
+			alert("Name is too short.");
+			return;
+		}
+		this._name = newName;
+	}
+
+	get description() {
+		return this._description;
+	}
+
+	set description(newDesc) {
+		if (newDesc.length < 1) {
+			alert("Description is too short.");
+			return;
+		}
+		this._description = newDesc;
+	}
+
+	get dueDate() {
+		return this._dueDate;
+	}
+
+	set dueDate(newDueDate) {
+		this._description = newDueDate;
+	}
+
+	get priority() {
+		return this._priority;
+	}
+
+	set priority(newPriority) {
+		this._description = newPriority;
+	}
+}
